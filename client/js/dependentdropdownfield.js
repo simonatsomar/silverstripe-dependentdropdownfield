66d9564ea0641e7607970d1af3ef820f71296361
Simon Carter <simon@somar.co.nz>
2025-05-14 14:49:07 +1200

    Fix form data serialization

--- client/js/dependentdropdownfield.js
+++ client/js/dependentdropdownfield.js
@@ -21,7 +21,7 @@ jQuery.entwine("dependentdropdown", function ($) {
 			depends.change(function () {
 				if (dependsForm) {
 					drop.updateOptions(
-						$.param(this.serializeArray())
+						$.param($(this).serializeArray())
 					);
 				} else if (!this.value) {
 					drop.disable(drop.data('unselected'));
